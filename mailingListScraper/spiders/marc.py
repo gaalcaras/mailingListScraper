da609ed74f612085ce243eab4e6dc5d49ec2cdba
batlab <gaby.alcaras@gmail.com>
2017-03-09 16:57:59 +0100

    Add first marc implementation and test cases

---
+++ mailingListScraper/spiders/marc.py
@@ -0,0 +1,129 @@
+# -*- coding: utf-8 -*-
+"""
+MarcSpider helps you scrap the mailing lists on the MARC archive: http://marc.info/.
+By default, it will only go through the git development list.
+"""
+
+import re
+import urllib
+
+import scrapy
+from scrapy.loader import ItemLoader
+
+from lxml import html
+
+from mailingListScraper.items import Email
+from mailingListScraper.spiders.ArchiveSpider import ArchiveSpider
+
+
+class MarcSpider(ArchiveSpider):
+    """
+    MarcSpider scraps the mailing lists in the MARC archive. When you create
+    a new instance of MarcSpider, it automatically goes through the index of
+    all mailing lists, which can then be scraped accordingly (if you instruct
+    it to do so of course).
+    """
+    name = "marc"
+    allowed_domains = ["marc.info"]
+    start_url = "http://marc.info/"
+
+    # Default list
+    default_list = 'git'
+
+    def _set_lists(self):
+        """
+        Extract path to messages lists for each mailing list.
+        """
+
+        res = urllib.request.urlopen(self.start_url).read()
+        body = html.fromstring(res)
+
+        a_names = body.xpath('//dd//a/text()')
+        a_hrefs = body.xpath('//dd//a/@href')
+        a_hrefs = [self.start_url + s for s in a_hrefs]
+
+        self.mailing_lists = dict(zip(a_names, a_hrefs))
+
+    def parse(self, response):
+        """
+        Extract all of the messages lists (grouped by date).
+
+        @url http://marc.info/?l=git&r=1&w=2
+        @returns requests 149
+        """
+        msglist_urls = response.xpath('//dd//@href').extract()
+        msglist_urls = [self.start_url + u for u in msglist_urls]
+
+        for url in msglist_urls:
+            yield scrapy.Request(url, callback=self.parse_msglist)
+
+    def parse_msglist(self, response):
+        """
+        Extract all relative URLs to the individual messages.
+
+        @url http://marc.info/?l=git&r=1&b=201406&w=2
+        @returns requests 31 31
+        """
+
+        xpath_next = "//pre//a[contains(text(), 'Next')][1]//@href"
+        next_url = response.xpath(xpath_next).extract()
+        next_url = self.start_url + next_url[0]
+
+        xpath_msg = "//pre//a[contains(@href, '&m')]//@href"
+        msg_urls = response.xpath(xpath_msg).extract()
+        msg_urls = [response.url + u for u in msg_urls]
+
+        for url in msg_urls:
+            yield scrapy.Request(url, callback=self.parse_item)
+
+        yield scrapy.Request(next_url, callback=self.parse_msglist)
+
+    def parse_item(self, response):
+        """
+        On the message page, extract the item.
+
+        @url http://marc.info/?l=git&m=148889722812631&w=2
+        @returns item 1
+        @scrapes url subject senderName senderEmail timeReceived body
+        """
+
+        load = ItemLoader(item=Email(), selector=response)
+
+        load.add_value('url', response.url)
+
+        title = response.xpath('//title/text()').extract()
+        reg_title = re.search("'(.*)'", title[0])
+        load.add_value('subject', reg_title.group(1))
+
+        xpath_replyto = "//a[contains(text(), 'prev in thread')]/@href"
+        replyto = response.xpath(xpath_replyto).extract()
+        if any(replyto):
+            load.add_value('replyto', self.start_url + replyto[0])
+
+        meta = response.xpath("//font[@size='+1']//child::text()").extract()
+        meta = ''.join(meta).split('\n') # Give each line its element in list
+        meta = list(filter(None, meta))[:-1] # Remove empty lines and last item
+
+        for line in meta:
+            reg = re.search('^([^:]*):(.*)$', line)
+
+            if "List" in reg.group(1):
+                load.add_value('mailingList', reg.group(2).strip())
+            elif 'From' in reg.group(1):
+                from_reg = re.search('(.*)<(.*)>', reg.group(2))
+
+                try:
+                    load.add_value('senderName', from_reg.group(1).strip())
+                    load.add_value('senderEmail', from_reg.group(2).strip())
+                except AttributeError:
+                    load.add_value('senderName', reg.group(2).strip())
+            elif 'Date' in reg.group(1):
+                load.add_value('timeReceived', reg.group(2).strip())
+
+        xpath_body = "//text()[preceding::b/font[@size='+1'] and following::b]"
+        body = response.xpath(xpath_body).extract()
+        load.add_value('body', ''.join(body))
+
+        load.add_value('timeSent', '')
+
+        return load.load_item()
---
+++ mailingListScraper/tests/marc.py
@@ -0,0 +1,90 @@
+# -*- coding: utf-8 -*-
+# pylint: disable=missing-docstring,protected-access
+"""
+Unit tests for MarcSpider.
+"""
+
+import unittest
+import re
+
+from mailingListScraper.tests.validation_case import ValidationCase
+from mailingListScraper.pipelines import CleanReplyto
+
+from mailingListScraper.spiders.marc import MarcSpider
+
+class TestBase(unittest.TestCase):
+    """
+    Creates a bunch of useful settings for testing spiders.
+    """
+
+    def setUp(self):
+        self.spider = MarcSpider()
+        self.cases = [
+            '20070731223613'
+            ]
+
+        # Enable long strings comparisons
+        self.maxDiff = None # pylint: disable=invalid-name
+
+class TestGetMailingList(TestBase):
+    """
+    Test if the spider gets all the mailing lists in the MARC archive.
+    """
+
+    def test_number_items(self):
+        self.spider._set_lists()
+        self.assertEqual(len(self.spider.mailing_lists), 3700)
+
+    def test_url_match(self):
+        self.spider._set_lists()
+        self.assertEqual(self.spider.mailing_lists['git'], 'http://marc.info/?l=git&r=1&w=2')
+        self.assertEqual(self.spider.mailing_lists['postgis-devel'],
+                         'http://marc.info/?l=postgis-devel&r=1&w=2')
+
+class TestItemExtraction(TestBase):
+    """
+    Testing how the Spider and the ItemLoaders deal with several test pages.
+    This test only concerns the Item extraction in the Spider (xpath, etc.)
+    and the ItemLoader stuff (input and output processor).
+    """
+
+    def test_item_extraction(self):
+        for case_id in self.cases:
+            with self.subTest(case_id=case_id):
+                # Create a validation case and generate test data
+                case = ValidationCase(case_id, self.spider.name)
+                test_item = self.spider.parse_item(case.response)
+
+                # Compare test and validation data
+                for item, true_value in case.raw_item.items():
+                    with self.subTest(item=item):
+                        self.assertEqual(true_value, test_item[item])
+
+    def test_body(self):
+        for case_id in self.cases:
+            with self.subTest(case_id=case_id):
+                # Create a validation case and generate test data
+                case = ValidationCase(case_id, self.spider.name)
+                test_item = self.spider.parse_item(case.response)
+
+                case_body = re.sub('\n$', '', str(case.body))
+                test_body = re.sub('\n$', '', test_item['body'])
+
+                self.assertEqual(test_body, case_body)
+
+class TestPipelines(TestBase):
+    """
+    Testing pipelines on "raw outputs" defined in the json test cases.
+    """
+
+    def test_clean_reply_to(self):
+        pipeline = CleanReplyto()
+
+        for case_id in self.cases:
+            with self.subTest(case_id=case_id):
+                # Create a validation case and generate test data
+                case = ValidationCase(case_id, self.spider.name)
+                test_item = pipeline.process_item(case.raw_item, self.spider)
+
+                # Compare test and validation data
+                self.assertEqual(case.final_item['replyto'], test_item['replyto'])
