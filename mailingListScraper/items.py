2d9be1794886d7885f8950bd22c5f01329a559e3
batlab <gaby.alcaras@gmail.com>
2017-03-13 16:04:01 +0100

    Change utf8 encoding for xml body

--- mailingListScraper/items.py
+++ mailingListScraper/items.py
@@ -1,40 +1,46 @@
 # -*- coding: utf-8 -*-
-# #############################################
-# Items and ItemLoaders
-# #############################################
-
+# pylint: disable=too-many-ancestors,unnecessary-pass
+"""
+Items and ItemLoaders
+"""
 import scrapy
 from scrapy.loader.processors import Join, MapCompose
 from w3lib.html import replace_entities
 
 
 class Email(scrapy.Item):
+    "The main item to store our emails"
+
     emailId = scrapy.Field()
 
     senderName = scrapy.Field(
-            input_processor=MapCompose(replace_entities),
-            output_processor=Join()
-            )
+        input_processor=MapCompose(replace_entities),
+        output_processor=Join()
+        )
     senderEmail = scrapy.Field(
-            input_processor=MapCompose(replace_entities),
-            output_processor=Join()
-            )
+        input_processor=MapCompose(replace_entities),
+        output_processor=Join()
+        )
 
     timeSent = scrapy.Field(
-            input_processor=MapCompose(replace_entities),
-            output_processor=Join()
-            )
+        input_processor=MapCompose(replace_entities),
+        output_processor=Join()
+        )
     timeReceived = scrapy.Field(
-            input_processor=MapCompose(replace_entities),
-            output_processor=Join()
-            )
+        input_processor=MapCompose(replace_entities),
+        output_processor=Join()
+        )
     timestampSent = scrapy.Field()
     timestampReceived = scrapy.Field()
 
+    body = scrapy.Field(
+        input_processor=MapCompose(replace_entities),
+        output_processor=Join(),
+        )
+
+    url = scrapy.Field(output_processor=Join())
     replyto = scrapy.Field(output_processor=Join())
     subject = scrapy.Field(output_processor=Join())
-    body = scrapy.Field(output_processor=Join())
-    url = scrapy.Field(output_processor=Join())
     mailingList = scrapy.Field(output_processor=Join())
 
     pass
